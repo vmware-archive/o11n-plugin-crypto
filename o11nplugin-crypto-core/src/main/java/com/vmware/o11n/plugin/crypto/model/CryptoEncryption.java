89c598a7874ad647b06b080431edf8fd0b40ec17
Daniel Linsley <dlinsley@vmware.com>
2017-04-30 20:43:04 -0600

    Make parameter descriptions more consistent Update Javadoc on service Allow for Secrets longer than 24 bytes

--- o11nplugin-crypto-core/src/main/java/com/vmware/o11n/plugin/crypto/model/CryptoEncryption.java
+++ o11nplugin-crypto-core/src/main/java/com/vmware/o11n/plugin/crypto/model/CryptoEncryption.java
@@ -25,7 +25,7 @@ import ch.dunes.vso.sdk.api.IPluginFactory;
 create=false,
 strict=true,
 singleton=true,
-description="Provides static methods to encrypt/decrypt data with different ciphers.")
+description="Provides static methods to encrypt/decrypt data with different ciphers. All ciphers use CBC mode with PKCS5 padding")
 public class CryptoEncryption {
 
 	public static final String TYPE = "CryptoEncryption";
@@ -37,20 +37,36 @@ public class CryptoEncryption {
 		return ((AbstractSpringPluginFactory) factory).createScriptingObject(CryptoEncryption.class);
 	}
 
-	@VsoMethod(description="AES Encryption. Returns Base64 encoded encrypted data")
+	@VsoMethod(description="AES Encryption. Returns encrypted data Base64 encoded")
 	public String aesEncrypt(
-			@VsoParam(description="Base64 encoded data to encrypt")String dataB64,
-			@VsoParam(description="Encryption secret Base64 encoded. For AES128 this should be 128 bits (16 bytes) long. For AES256 this should be 256 bits (32 bytes) long.")String secretB64,
-			@VsoParam(description="Base64 encoded initialization vector. 16 bytes long")String ivB64) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+			@VsoParam(description="Data to encrypt Base64 encoded") String dataB64,
+			@VsoParam(description="Encryption secret Base64 encoded. For AES128 this should be 128 bits (16 bytes) long. For AES256 this should be 256 bits (32 bytes) long.") String secretB64,
+			@VsoParam(description="Initialization Vector Base64 encoded. 16 bytes long") String ivB64) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
 		return service.aesEncrypt(dataB64, secretB64, ivB64);
 	}
 
-	@VsoMethod(description="AES Decryption. Returns Base64 encoded data")
+	@VsoMethod(description="AES Decryption. Returns data Base64 encoded")
 	public String aesDecrypt(
-			@VsoParam(description="Base64 encoded encrypted data to decrypt")String encryptedB64,
-			@VsoParam(description="Encryption secret Base64 encoded. For AES128 this should be 128 bits (16 bytes) long. For AES256 this should be 256 bits (32 bytes) long.")String secretB64,
-			@VsoParam(description="Base64 encoded initialization vector. 16 bytes long")String ivB64) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+			@VsoParam(description="Data to decrypt Base64 encoded.") String encryptedB64,
+			@VsoParam(description="Encryption secret Base64 encoded. For AES128 this should be 128 bits (16 bytes) long. For AES256 this should be 256 bits (32 bytes) long.") String secretB64,
+			@VsoParam(description="Initialization vector Base64 encoded. 16 bytes long") String ivB64) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
 		return service.aesDecrypt(encryptedB64, secretB64, ivB64);
+	}
+
+	@VsoMethod(description="3DES Encryption. Returns encrypted data Base64 encoded")
+	public String tripleDesEncrypt(
+			@VsoParam(description="Data to encrypt Base64 encoded") String dataB64,
+			@VsoParam(description="Encryption secret Base64 encoded. 192 bits (24 bytes) long. If longer, only the first 24 bytes will be used.") String secretB64,
+			@VsoParam(description="Base64 encoded initialization vector. 8 bytes long") String ivB64) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+		return service.tripleDesEncrypt(dataB64, secretB64, ivB64);
+	}
+
+	@VsoMethod(description="3DES Decryption. Returns data Base64 encoded")
+	public String tripleDesDecrypt(
+			@VsoParam(description="Data to decrypt Base64 encoded.") String encryptedB64,
+			@VsoParam(description="Encryption secret Base64 encoded. 192 bits (24 bytes) long. If longer, only the first 24 bytes will be used.") String secretB64,
+			@VsoParam(description="Base64 encoded initialization vector. 8 bytes long") String ivB64) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+		return service.tripleDesDecrypt(encryptedB64, secretB64, ivB64);
 	}
 
 	@VsoMethod(description="Returns 16 random bytes encoded as a Base64 string suitable for an AES Initialization Vector")
--- o11nplugin-crypto-core/src/main/java/com/vmware/o11n/plugin/crypto/model/CryptoEncryptionService.java
+++ o11nplugin-crypto-core/src/main/java/com/vmware/o11n/plugin/crypto/model/CryptoEncryptionService.java
@@ -14,6 +14,7 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.DESedeKeySpec;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
@@ -27,11 +28,12 @@ public class CryptoEncryptionService {
 	private static final String DESEDE_CIPHER = "DESede/CBC/PKCS5Padding"; //3DES
 
 	/**
+	 * AES Encryption CBC Mode with PKCS5 Padding
 	 *
-	 * @param dataB64
-	 * @param secretB64
-	 * @param ivB64
-	 * @return
+	 * @param dataB64 Data to encrypt Base64 encoded
+	 * @param secretB64 Encryption secret Base64 encoded. For AES128 this should be 128 bits (16 bytes) long. For AES256 this should be 256 bits (32 bytes) long.
+	 * @param ivB64 Initialization Vector Base64 encoded. 16 bytes long
+	 * @return Encrypted data Base64 Encoded
 	 * @throws NoSuchAlgorithmException
 	 * @throws NoSuchPaddingException
 	 * @throws InvalidKeyException
@@ -54,11 +56,12 @@ public class CryptoEncryptionService {
 		return encryptedB64;
 	}
 	/**
+	 * AES Decryption CBC Mode with PKCS5 Padding
 	 *
-	 * @param encryptedB64
-	 * @param secretB64
-	 * @param ivB64
-	 * @return
+	 * @param encryptedB64 Encrypted Data Base64 encoded.
+	 * @param secretB64 Encryption secret Base64 encoded. For AES128 this should be 128 bits (16 bytes) long. For AES256 this should be 256 bits (32 bytes) long.
+	 * @param ivB64 Initialization Vector Base64 encoded. 16 bytes long
+	 * @return Original data Base64 encoded.
 	 * @throws NoSuchAlgorithmException
 	 * @throws NoSuchPaddingException
 	 * @throws InvalidKeyException
@@ -82,11 +85,12 @@ public class CryptoEncryptionService {
 	}
 
 	/**
+	 * TripleDES (EDE) Encryption CBC Mode with PKCS5 padding
 	 *
-	 * @param dataB64
-	 * @param secretB64
-	 * @param ivB64
-	 * @return
+	 * @param dataB64 Data to encrypt Base64 encoded.
+	 * @param secretB64 Encryption secret Base64 encoded. Secret must be at least 24 bytes. Only the first 24 bytes will be used.
+	 * @param ivB64 Initialization Vector Base64 encoded. Only first 8 bytes will be used.
+	 * @return Encrypted data Base64 encoded.
 	 * @throws NoSuchAlgorithmException
 	 * @throws NoSuchPaddingException
 	 * @throws InvalidKeyException
@@ -103,17 +107,19 @@ public class CryptoEncryptionService {
 		final byte[] ivBytes = Base64.decodeBase64(ivB64);
 		final Cipher cipher = Cipher.getInstance(DESEDE_CIPHER);
 
-		cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(secretBytes, "DESede"), new IvParameterSpec(ivBytes, 0, cipher.getBlockSize()));
+		DESedeKeySpec keySpec = new DESedeKeySpec(secretBytes);
+		cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(keySpec.getKey(), "DESede"), new IvParameterSpec(ivBytes, 0, cipher.getBlockSize()));
 
 		encryptedB64 = Base64.encodeBase64String(cipher.doFinal(dataBytes));
 		return encryptedB64;
 	}
 	/**
+	 * TripleDES (EDE) Decryption CBC Mode with PKCS5 padding
 	 *
-	 * @param encryptedB64
-	 * @param secretB64
-	 * @param ivB64
-	 * @return
+	 * @param encryptedB64 Encrypted data Base64 encoded
+	 * @param secretB64 Encryption secret Base64 encoded. Secret must be at least 24 bytes. Only the first 24 bytes will be used.
+	 * @param ivB64 Initialization Vector Base64 encoded. Only first 8 bytes will be used.
+	 * @return Original data Base64 encoded.
 	 * @throws NoSuchAlgorithmException
 	 * @throws NoSuchPaddingException
 	 * @throws InvalidKeyException
@@ -130,15 +136,20 @@ public class CryptoEncryptionService {
 		final byte[] ivBytes = Base64.decodeBase64(ivB64);
 		final Cipher cipher = Cipher.getInstance(DESEDE_CIPHER);
 
-		cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(secretBytes, "DESede"), new IvParameterSpec(ivBytes, 0, cipher.getBlockSize()));
+		DESedeKeySpec keySpec = new DESedeKeySpec(secretBytes);
+		cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(keySpec.getKey(), "DESede"), new IvParameterSpec(ivBytes, 0, cipher.getBlockSize()));
 
 		dataB64 = Base64.encodeBase64String(cipher.doFinal(encryptedBytes));
 		return dataB64;
 	}
 
 	/**
+	 * Random data generator
 	 *
-	 * @return
+	 * Creates a new SecureRandom instance for each all.  Each instance is self seeding.
+	 *
+	 * @param numberOfBytes Number of Random Bytes to return
+	 * @return Random bytes Base64 Encoded
 	 */
 	public String generateRandomBytes(Integer numberOfBytes) {
 		byte[] ivBytes = new byte[numberOfBytes];
