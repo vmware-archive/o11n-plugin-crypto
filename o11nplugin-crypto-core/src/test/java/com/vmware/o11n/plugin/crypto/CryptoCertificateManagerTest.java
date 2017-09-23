57e8cc646ff383faf1eda57a12725599665951e0
Daniel Linsley <dlinsley@vmware.com>
2017-09-23 16:59:03 -0700

    add missing SPDX id
    Signed-off-by: Daniel Linsley <dlinsley@vmware.com>

--- o11nplugin-crypto-core/src/test/java/com/vmware/o11n/plugin/crypto/CryptoCertificateManagerTest.java
+++ o11nplugin-crypto-core/src/test/java/com/vmware/o11n/plugin/crypto/CryptoCertificateManagerTest.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017 VMware, Inc. All Rights Reserved.
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
 package com.vmware.o11n.plugin.crypto;
 
 public class CryptoCertificateManagerTest {
